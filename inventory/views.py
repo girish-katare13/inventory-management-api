5a69a9dcd4a53aaeea742dc7bf77b984a25f082e
girish-katare13 <girishkatare13@gmail.com>
2026-06-19 13:38:09 +0530

    Add product detail update and delete endpoints

--- inventory/views.py
+++ inventory/views.py
@@ -9,3 +9,7 @@ from .serializers import ProductSerializer
 class ProductListCreateView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
