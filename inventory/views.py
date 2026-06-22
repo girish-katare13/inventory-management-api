76e7c2d0efcb727b6eac3121a7738457f90f5f06
girish-katare13 <girishkatare13@gmail.com>
2026-06-22 14:00:50 +0530

    Add product filtering support

--- inventory/views.py
+++ inventory/views.py
@@ -4,11 +4,14 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Product
 from .serializers import ProductSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class ProductListCreateView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['name']
 
 class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
